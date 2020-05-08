75754af94059348f10232a0e5c4cadd0595c6263
devonkhosa <dalvinderkhosa@gmail.com>
2020-05-07 23:49:44 -0700

    Added electron

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const {spawn} = require("child_process");
 
 const ls = spawn("ls", ["-la"]);
 
+const electron = require("electron");
+
 ls.stdout.on("data", data => {
     console.log(`stdout: ${data}`);
 });
+
+
